89425424fefb46f9ea6383c5cd2075f9c4e9597f
antonio <dimon085496@gmail.com>
2025-05-12 14:38:13 +1000

    <Ваше сÐстраничка с резюме с использованием шаблона

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  django.views.generic import TemplateView
 # Create your views here.
-def home_page_view(request):
-    return HttpResponse("hello")
+class HomePageView(TemplateView):
+   template_name = "resume.html"
